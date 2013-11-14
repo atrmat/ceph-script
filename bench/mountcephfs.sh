9fc3b298d860e8f42895c24810be475fd586d767
atrmat <atrmat@users.noreply.github.com>
2013-11-14 17:26:55 +0800

    more flexible
    test under ceph 0.67.4

--- bench/mountcephfs.sh
+++ bench/mountcephfs.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# test under ceph 0.67.4
+cephfs_mnt="/mnt/cephfs/"
+if [ ! -d "$cephfs_mnt" ]
+then
+        mkdir -p "$cephfs_mnt"
+fi
+mount -t ceph `cat /etc/hostname`:6789:/ /mnt/cephfs -vv -o name=admin,secret=`cat /etc/ceph/keyring.admin | grep "key" | gawk -F " " '{gsub(/[[:blank:]]*/,"",$3);print $3}'`
 
-mount -t ceph ceph08:6789:/ /mnt/cephfs -vv -o name=admin,secret=AQC/wsFRyDpjEhAAuX+XzULqIz4pZ3q5QkCdWw==
