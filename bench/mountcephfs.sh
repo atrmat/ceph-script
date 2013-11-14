db5c4dde1c6c5c81a03a43af0bb4cd206d4e29fd
atrmat <atrmat@users.noreply.github.com>
2013-11-14 17:31:33 +0800

    some modified

--- bench/mountcephfs.sh
+++ bench/mountcephfs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # test under ceph 0.67.4
-cephfs_mnt="/mnt/cephfs/"
+
+cephfs_mnt="/mnt/cephfs"
 if [ ! -d "$cephfs_mnt" ]
 then
         mkdir -p "$cephfs_mnt"
+else
+        umount "$cephfs_mnt"
 fi
-mount -t ceph `cat /etc/hostname`:6789:/ /mnt/cephfs -vv -o name=admin,secret=`cat /etc/ceph/keyring.admin | grep "key" | gawk -F " " '{gsub(/[[:blank:]]*/,"",$3);print $3}'`
-
+mount -t ceph `cat /etc/hostname`:6789:/ "$cephfs_mnt" -vv -o name=admin,secret=`cat /etc/ceph/keyring.admin | grep "key" | gawk -F " " '{gsub(/[[:blank:]]*/,"",$3);print $3}'`
